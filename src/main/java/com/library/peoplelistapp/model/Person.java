1c23a881f1995894c9a49bcad0ea7f9606fa14b1
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-09 22:42:06 -0300

    add getters  and setters

--- src/main/java/com/library/peoplelistapp/model/Person.java
+++ src/main/java/com/library/peoplelistapp/model/Person.java
@@ -19,4 +19,36 @@ public class Person {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
