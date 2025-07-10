f925407663037d043cba3bccd583c8b6f0d6a43c
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-09 22:40:35 -0300

    add person class

--- src/main/java/com/library/peoplelistapp/model/Person.java
+++ src/main/java/com/library/peoplelistapp/model/Person.java
@@ -1,4 +1,22 @@
 package com.library.peoplelistapp.model;
 
 public class Person {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private static int personNumber = 0;
+
+    public Person() {
+        this.id = ++personNumber;
+    }
+
+
+    public Person(int id, String name, String phoneNumber, String email) {
+        this();// Automatically increments the id
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
