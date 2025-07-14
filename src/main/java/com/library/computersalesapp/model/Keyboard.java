96047a3ed643dd893c52a40fab1ad521fe2c1dc5
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-14 09:25:29 -0300

    reformat code

--- src/main/java/com/library/computersalesapp/model/Keyboard.java
+++ src/main/java/com/library/computersalesapp/model/Keyboard.java
@@ -1,18 +1,16 @@
 package com.library.computersalesapp.model;
 
 public class Keyboard extends InputDevice {
-    private final int idKeyboard;
-    private static int keyboardCounter;
+  private final int idKeyboard;
+  private static int keyboardCounter;
 
-    public Keyboard(String inputType, String brand) {
-        super(inputType, brand);
-        idKeyboard = ++keyboardCounter;
-    }
+  public Keyboard(String inputType, String brand) {
+    super(inputType, brand);
+    idKeyboard = ++keyboardCounter;
+  }
 
-    @Override
-    public String toString() {
-        return "Keyboard{" +
-                "idKeyboard=" + idKeyboard +
-                "} " + super.toString();
-    }
+  @Override
+  public String toString() {
+    return "Keyboard{" + "idKeyboard=" + idKeyboard + "} " + super.toString();
+  }
 }
--- src/main/java/com/library/computersalesapp/model/Mouse.java
+++ src/main/java/com/library/computersalesapp/model/Mouse.java
@@ -1,18 +1,16 @@
 package com.library.computersalesapp.model;
 
 public class Mouse extends InputDevice {
-    private final int idMouse;
-    private static int mouseCounter;
+  private final int idMouse;
+  private static int mouseCounter;
 
-    public Mouse(String inputType, String brand) {
-        super(inputType, brand);
-        idMouse = ++mouseCounter;
-    }
+  public Mouse(String inputType, String brand) {
+    super(inputType, brand);
+    idMouse = ++mouseCounter;
+  }
 
-    @Override
-    public String toString() {
-        return "Mouse{" +
-                "idMouse=" + idMouse +
-                "} " + super.toString();
-    }
+  @Override
+  public String toString() {
+    return "Mouse{" + "idMouse=" + idMouse + "} " + super.toString();
+  }
 }
