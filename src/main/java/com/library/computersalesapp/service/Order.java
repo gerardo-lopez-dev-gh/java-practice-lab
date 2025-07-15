2a9b110d17c672fcc1a3084b857b054df90e9311
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-15 17:26:00 -0300

    add new class Order

--- src/main/java/com/library/computersalesapp/service/Order.java
+++ src/main/java/com/library/computersalesapp/service/Order.java
@@ -1,4 +1,35 @@
 package com.library.computersalesapp.service;
 
+import com.library.computersalesapp.model.Computer;
+import java.util.List;
+
 public class Order {
+    private final int idOrder;
+    private final List<Computer> computerList;
+    private static int orderCount = 0;
+
+    public Order() {
+        this.idOrder = ++orderCount;
+        this.computerList = new java.util.ArrayList<>();
+    }
+
+    public void addComputer(Computer computer) {
+        if (computer != null) {
+            computerList.add(computer);
+        } else {
+            throw new IllegalArgumentException("Computer cannot be null");
+        }
+    }
+
+    public void showOrder() {
+        System.out.println("Order ID: " + idOrder);
+        if (computerList.isEmpty()) {
+            System.out.println("No computers in this order.");
+        } else {
+            System.out.println("Computers in this order:");
+            for (Computer computer : computerList) {
+                System.out.println(computer);
+            }
+        }
+    }
 }
