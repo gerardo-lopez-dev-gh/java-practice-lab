563c226e5927cdf0b9cc8aa4c0a8692a0e814d79
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-15 08:52:45 -0300

    add new main class ComputerSalesApp

---
+++ src/main/java/com/library/computersalesapp/ComputerSalesApp.java
@@ -0,0 +1,7 @@
+package com.library.computersalesapp;
+
+public class ComputerSalesApp {
+  public static void main(String[] args) {
+    System.out.println("Hello, welcome to the Computer Sales Application!");
+  }
+}
